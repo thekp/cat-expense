12cd41f5240ba814890d2bc28493343835993b0d
thekp <thekp97@gmail.com>
2023-08-19 17:51:40 +0700

    Add link to code in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,12 +14,16 @@ export const Footer = (props: FlexProps) => {
 			bottom={0}
 			py={3}
 			color="white"
+			textAlign="center"
 			_dark={{
 				color: 'black',
 			}}
 			{...props}
 		>
-			<Flex margin="0 auto">
+			<Flex margin="0 auto" direction="column" alignItems="center">
+				<ChakraLink isExternal href="https://github.com/thekp/cat-expense" flexGrow={1} mr={2} display="flex" alignItems="center">
+					See the code here <LinkIcon pl="4px" />
+				</ChakraLink>
 				<ChakraLink isExternal href="https://thekp.dev/" flexGrow={1} mr={2} display="flex" alignItems="center">
 					<Image src="/khoa.png" alt="Close up of Khoa's face" width={20} height={20} style={{ paddingRight: '4px' }} />
 					Created by Khoa Phan <LinkIcon pl="4px" />
